4cd576cb7c50dd05d40808705d71c05301a9439c
mekshitijsingh <91209593+mekshitijsingh@users.noreply.github.com>
2022-09-20 14:36:03 +0530

    Update Problem1_Sep20.cpp

--- DAA/Problem1_Sep20.cpp
+++ DAA/Problem1_Sep20.cpp
@@ -1,77 +1,75 @@
-// You are using GCC
-#include <iostream>
-// #include <cstdio>
+
+#include<iostream>
+//#include<cstdlib>
 using namespace std;
 
-struct node{
+struct node {
     int data;
-    struct node* left, *right;
+    struct node *left, *right;
 };
 
-struct node* newnode(int data){
-    struct node* temp;
-    temp = (struct node*)malloc(sizeof(struct node));
-    temp->data = data;
-    temp->left = temp->right = NULL;
+struct node *newnode(int data) {
+    struct node *temp;
+    temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=data;
+    temp->left=temp->right=NULL;
     return temp;
 }
 
-struct node* insert(struct node* node, int data){
-    if(node==NULL){
+struct node* insert(struct node *node, int data) {
+    if(node==NULL) {
         return newnode(data);
     }
-    else if(data<node->data){
-        node->left = insert(node->left, data);
+    else if(data<node->data) {
+        node->left=insert(node->left,data);
     }
-    else{
-        node->right = insert(node->right, data);
+    else {
+        node->right=insert(node->right, data);
     }
     return node;
 }
 
-void inorder(struct node* root){
-    if(root!=NULL){
+void inorder(struct node *root) {
+    if(root!=NULL) {
         inorder(root->left);
         cout<<root->data<<" ";
         inorder(root->right);
     }
 }
 
-void preorder(struct node* root){
-    if(root!=NULL){
+void preorder(struct node *root) {
+    if(root!=NULL) {
         cout<<root->data<<" ";
         preorder(root->left);
         preorder(root->right);
     }
 }
 
-void postorder(struct node* root){
-    if(root!=NULL){
+void postorder(struct node *root) {
+    if(root!=NULL) {
         postorder(root->left);
         postorder(root->right);
         cout<<root->data<<" ";
-    }    
+    }
 }
 
-int main(){
-    struct node* root = NULL;
-    int n, data;
+int main() {
+    struct node *root=NULL;
+    int n,data;
     cin>>n;
     cin>>data;
-    root = insert(root,data);
-    for(int i=0; i<n-1; i++){
+    root=insert(root,data);
+    for(int i=0; i<n; i++) {
         cin>>data;
         insert(root, data);
     }
-    
     cout<<"Inorder: ";
     inorder(root);
     
     cout<<"\nPreorder: ";
     preorder(root);
     
-    cout<<"\nPostorder: ";
+    cout<<"\n Postorder: ";
     postorder(root);
-    
     return 0;
 }
