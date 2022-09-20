92e0d7f55d16dc7315d9e2f6941fce00e7a4e857
Justin T Joseph <93005964+just-injoey@users.noreply.github.com>
2022-09-20 14:31:52 +0530

    Create Problem1_Sep20.cpp

---
+++ DAA/Problem1_Sep20.cpp
@@ -0,0 +1,77 @@
+// You are using GCC
+#include <iostream>
+// #include <cstdio>
+using namespace std;
+
+struct node{
+    int data;
+    struct node* left, *right;
+};
+
+struct node* newnode(int data){
+    struct node* temp;
+    temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+struct node* insert(struct node* node, int data){
+    if(node==NULL){
+        return newnode(data);
+    }
+    else if(data<node->data){
+        node->left = insert(node->left, data);
+    }
+    else{
+        node->right = insert(node->right, data);
+    }
+    return node;
+}
+
+void inorder(struct node* root){
+    if(root!=NULL){
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+}
+
+void preorder(struct node* root){
+    if(root!=NULL){
+        cout<<root->data<<" ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct node* root){
+    if(root!=NULL){
+        postorder(root->left);
+        postorder(root->right);
+        cout<<root->data<<" ";
+    }    
+}
+
+int main(){
+    struct node* root = NULL;
+    int n, data;
+    cin>>n;
+    cin>>data;
+    root = insert(root,data);
+    for(int i=0; i<n-1; i++){
+        cin>>data;
+        insert(root, data);
+    }
+    
+    cout<<"Inorder: ";
+    inorder(root);
+    
+    cout<<"\nPreorder: ";
+    preorder(root);
+    
+    cout<<"\nPostorder: ";
+    postorder(root);
+    
+    return 0;
+}
