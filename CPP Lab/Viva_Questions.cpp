fb3c68c61b9dc00f210f9a224da1604cd031a6dd
Justin T Joseph <93005964+just-injoey@users.noreply.github.com>
2022-10-06 14:26:53 +0530

    Create Viva_Questions.cpp

---
+++ CPP Lab/Viva_Questions.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x = 018;
+    cout<<x;    
+
+    // unsigned int
+
+    switch(printf("xyz\n")){
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        default:
+    }
+
+    if(printf('J')){
+        printf("yes");
+    }
+
+    if(printf('J\0')){
+        printf("yes");
+    }
+
+    if(printf('\0')){
+        printf("yes");
+    }
+
+    if(prinf(NULL)){
+        printf("yes");
+    }
+    else{
+        printf("No");
+    }
+
+    
+
+    if('\0'){
+        cout<<"Helllo"<<endl;
+    }
+
+
+    if("\0"){
+        cout<<"Helllo"<<endl;
+    }
+
+
+    if('\0'){
+        cout<<"Helllo"<<endl;
+    }
+
+
+    // Output: error
+    
+    // what are biwise operator
+    
+    switch()
+
+    if('+'){
+        cout<<"yes"<<endl;
+    }
+
+    auto a = 1&&(-10);
+    cout<<a<<endl;
+
+    x = 10;
+    cout<<x++;
+    cout<<++x;
+
+
+    
+    return 0;
+
+}
