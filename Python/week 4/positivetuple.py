cc6754de642f7587c15de2ea7fad727dbb2db2be
Niteesh Kumar Pandey <98735160+NiteeshK21@users.noreply.github.com>
2022-11-02 10:03:06 +0530

    Update positivetuple.py

--- Python/week 4/positivetuple.py
+++ Python/week 4/positivetuple.py
@@ -1,14 +1,10 @@
-# You are using Python
 n=int(input())
-numbers_list=[]
+List=[]
 for i in range(n):
-    numbers_list.append(int(input()))
-numbers_tuple =tuple(numbers_list)
-positive_list=[]
-for num in numbers_list:
-    if num>0:
-        positive_list.append(num)
-        
-positive_tuple=tuple(positive_list)
-print(f"The elements in the tuple are: {numbers_tuple}")
-print(positive_tuple)
+    List.append(int(input()))
+print("The elements in the tuple are: ",tuple(List))
+List1=[]
+for i in List:
+    if(i > 0):
+        List1.append(i)
+print(tuple(List1))
