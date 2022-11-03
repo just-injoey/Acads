465c040ddc6e0608d3048baf4cdbfcea623f12a6
Justin T Joseph <93005964+just-injoey@users.noreply.github.com>
2022-11-03 22:43:49 +0530

    Update numberofstrings.py

--- Python/week 4 challenger/numberofstrings.py
+++ Python/week 4 challenger/numberofstrings.py
@@ -1,8 +1,21 @@
 # You are using Python
+# You are using Python
 n=str(input())
 count=0
 string_list=input().split()
 for string in string_list:
-    if len(string) >=2 and string[0]==string[len(string)-1]:
+    if len(string) >2 and string[0]==string[len(string)-1]:
         count+=1
 print(count)
+
+
+
+
+############### Wrong code
+# n=str(input())
+# count=0
+# string_list=input().split()
+# for string in string_list:
+#     if len(string) >=2 and string[0]==string[len(string)-1]:
+#         count+=1
+# print(count)
