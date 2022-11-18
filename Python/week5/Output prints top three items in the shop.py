0eb567d5d07db631d32493721627b3bf355ab2d7
C.Lokesh Swarup <111900256+lokeshswarup@users.noreply.github.com>
2022-11-18 09:45:21 +0530

    Create Output prints top three items in the shop.py

---
+++ Python/week5/Output prints top three items in the shop.py
@@ -0,0 +1,10 @@
+from heapq import nlargest
+from operator import itemgetter
+n = int(input())
+items = {}
+for i in range(n):
+    dict1 = {}
+    inp = input().split(':')
+    items[inp[0]] = inp[1]
+for name, value in nlargest(3, items.items(), key=itemgetter(1)):
+    print(name, value)
