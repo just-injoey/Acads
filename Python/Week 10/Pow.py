555744f15c86e5ad661162ccfc1c6d66c712a608
Justin T Joseph <93005964+just-injoey@users.noreply.github.com>
2022-11-18 10:29:18 +0530

    Create Pow.py

---
+++ Python/Week 10/Pow.py
@@ -0,0 +1,11 @@
+# You are using Python
+class Pow():
+    def __init__(self, x, n):
+        self.x = x
+        self.n = n
+    def powe(self):
+        return x**n
+x = int(input())
+n = int(input())
+p = Pow(x,n)
+print(p.powe())
