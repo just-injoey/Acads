59038512f16b7067999a0e1b5a913c88fa6b9b25
C.Lokesh Swarup <111900256+lokeshswarup@users.noreply.github.com>
2022-11-18 09:54:57 +0530

    Create print_all_unique_values_in_dictionary.py

---
+++ Python/week5/print_all_unique_values_in_dictionary.py
@@ -0,0 +1,9 @@
+n=int(input())
+stat=[]
+for i in range(n):
+    dict1 = {}
+    inp = input().split(':')
+    dict1[inp[0]] = inp[1]
+    stat.append(dict1)
+u_value = set( val for dict1 in stat for val in dict1.values())
+print(sorted((set(u_value))))
